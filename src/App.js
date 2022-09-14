f4897c3a92230890218d43422860a1a8c8b916c4
Nathan Zeager <nathanzeager@hotmail.com>
2022-09-14 12:32:59 -0400

    Added API call to display question categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Trivia Game</h1>
+      <DisplayCategories />
+      
+    </>
   );
+}
+
+
+const DisplayCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    console.log('useEffect runs')
+    axios.get('https://opentdb.com/api_category.php').then((res) => setCategories(res.data.trivia_categories))
+  }, [])
+
+  return (
+    <>
+      <h2>Select a Category</h2>
+      <div className='cat-list'>
+        {console.log(categories)}
+        {categories.map((cat) => (
+          <div><button key={cat.id}> {cat.name} </button></div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default App;
