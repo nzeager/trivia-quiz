1d092adc7f217eb79d6b03583bc1dd85df926851
Nathan Zeager <nathanzeager@hotmail.com>
2022-09-17 12:13:38 -0400

    Added return for quiz page after final question is answered

--- src/TakeQuiz.js
+++ src/TakeQuiz.js
@@ -22,40 +22,55 @@ export const TakeQuiz = ({catID, setCatID}) => {
 
     if (questions.length > 0) {
         let len = questions.length
-        let category = questions[questIndex].category
+        let category = questions[0].category
 
-        let question = questions[questIndex].question
-        let correctAnswer = questions[questIndex].correct_answer
-
-        let options=[]
-        options=[...questions[questIndex].incorrect_answers,questions[questIndex].correct_answer]
-
-
+        
+        
         // let correctIndex = options.findIndex(questions[questIndex].correct_answer)
-
+        
+        if (questIndex < len) {
+            let question = questions[questIndex].question
+            let correctAnswer = questions[questIndex].correct_answer
+    
+            let options=[]
+            options=[...questions[questIndex].incorrect_answers,questions[questIndex].correct_answer]
+            return(
+                <>
+                    <div className='title'> 
+                        {category} Quiz 
+                    </div>
+                    <div className='score'>
+                        Score: {score}
+                    </div>
+                    <div className='question'> 
+                        Question {questIndex+1}: {question}
+                    </div>
+                    {options.map((option) => (
+                        <div onClick={() => {
+                            setQuestIndex(questIndex+1);
+                            (option === correctAnswer) ? setScore(score + 1) : setScore(score);
+                        }}> 
+                            {option}
+                        </div>
+                    ))}
+                    <div>
+                        <button onClick={() => {setCatID(null)}}> Return to Categories </button>
+                    </div>
+                    {console.log(questions)}
+                </>
+            )
+        }
         return(
             <>
                 <div className='title'> 
                     {category} Quiz 
                 </div>
                 <div className='score'>
-                    Score: {score}
+                    Final Score: {score}
                 </div>
-                <div className='question'> 
-                    Question {questIndex+1}: {question}
-                </div>
-                {options.map((option) => (
-                    <div onClick={() => {
-                        setQuestIndex(questIndex+1);
-                        (option === correctAnswer) ? setScore(score + 1) : setScore(score);
-                    }}> 
-                        {option}
-                    </div>
-                ))}
                 <div>
                     <button onClick={() => {setCatID(null)}}> Return to Categories </button>
                 </div>
-                {console.log(questions)}
             </>
         )
     }
